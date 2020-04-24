42348b2e073f549c29d477aa2447e1d7a556ddeb
kkgorski <jgorski63@gmail.com>
2020-04-24 19:23:37 +0200

    [Linked list] Removal of SIZE template parameter, class was renamed to start with capital letter

--- linkedList/LinkedList.cpp
+++ linkedList/LinkedList.cpp
@@ -2,12 +2,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
 
-template<typename TYPE, std::size_t SIZE>
-class linkedList
+template<typename TYPE>
+class LinkedList
 {
 };
-
-TEST_CASE( "Linked list is constructed") {
-    REQUIRE( true );
-}
 
